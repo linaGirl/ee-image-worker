d8a2a4e08a151a930aee6400ee8476cf94ed5a22
Michael Rüfenacht <ruefe@rcky.ch>
2013-11-09 08:50:54 +0100

    Working on the images, not too trivial thou! Added a simple testing suite.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+var WorkerImage = require('./lib/WorkerImage'),
+    fs          = require('fs');
+
+module.exports = require('./lib/ImageWorker');
 
 
+fs.readFile('./images/theband.jpg', function(err, buffer){
+    console.log(err);
+    var img = new WorkerImage(buffer);
+    console.log(img.stat());
 
-	module.exports = require('./lib/ImageWorker');
+});
--- test.js
+++ test.js
@@ -1,10 +1,58 @@
+var   Class 		= require('ee-class')
+    , log 			= require('ee-log')
+    , assert 		= require('assert');
 
+var testCases       = {};
 
+var ImageWorker 	= require('./');
 
-	var   Class 		= require('ee-class')
-		, log 			= require('ee-log')
-		, assert 		= require('assert');
+var WorkerImage = require('./lib/WorkerImage');
 
+testCases.crop = {
+    _setup: function(){
+        this.img = new WorkerImage(null, {});
+    }
 
+    , cropSingle: function(){
+        assert.equal(this.img.top, 0);
+        assert.equal(this.img.left, 0);
+        assert.equal(this.img.width, 0);
+        assert.equal(this.img.height, 0);
 
-	var ImageWorker 	= require('./');
+        this.img.crop({top: 100, left: 200, width: 300, height: 400});
+
+        assert.equal(this.img.top, 100);
+        assert.equal(this.img.left, 200);
+        assert.equal(this.img.width, 300);
+        assert.equal(this.img.height, 400);
+
+        assert.equal(this.img.actions.length, 1);
+    }
+
+    , cropChained: function(){
+        this.img.crop({top: 100, left: 200, width: 300, height: 400})
+                .crop({top:100, left: 50, width: 200, height: 200});
+
+        assert.equal(this.img.top, 200);
+        assert.equal(this.img.left, 250);
+        assert.equal(this.img.width, 200);
+        assert.equal(this.img.height, 200);
+
+        assert.equal(this.img.actions.length, 2);
+    }
+};
+
+for(var name in testCases){
+    log.info('Running suite: '+name)
+    for(var test in testCases[name]){
+        if(test[0]!=='_'){
+            try{
+                testCases[name]._setup();
+                testCases[name][test].call(testCases[name]);
+                log.info('['+test+'] executed successfully.');
+            } catch (e) {
+                log.error('['+test+'] failed with message: "'+ e.message+'"')
+            }
+        }
+    }
+}
