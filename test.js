a44342fc6aeb365dafc583c6f8b204458fb5cd1a
Michael Rüfenacht <ruefe@rcky.ch>
2013-11-21 18:23:20 +0100

    Making progress. Chaining now works but needs tests for the wrapped picha functions.

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
 var   Class 		= require('ee-class')
     , log 			= require('ee-log')
     , assert 		= require('assert')
-    , CallStack     = require('./lib/CallStack');
+    , FluentInterface     = require('./lib/FluentInterface')
+    , picha         = require('picha');
 
 var testCases       = {};
-
-var ImageWorker 	= require('./');
-
 var WorkerImage = require('./lib/WorkerImage');
 
 testCases.crop = {
     _setup: function(){
-        this.img = new WorkerImage(null, {});
+        this.img = new WorkerImage();
         // have to modify that manually for now
         this.img.width = 800;
         this.img.height = 600;
@@ -30,33 +28,73 @@ testCases.crop = {
         assert.equal(this.img.width, 300);
         assert.equal(this.img.height, 400);
 
-        assert.equal(this.img.actions.length, 1);
+        assert.equal(this.img.length, 1);
     }
 
     , cropChained: function(){
         this.img.crop({top: 100, left: 200, width: 300, height: 400})
                 .crop({top:100, left: 50, width: 200, height: 200});
 
+        this.img.setWrapped(new picha.Image({data:new Buffer(0)}));
+
         assert.equal(this.img.top, 200);
         assert.equal(this.img.left, 250);
         assert.equal(this.img.width, 200);
         assert.equal(this.img.height, 200);
 
-        assert.equal(this.img.actions.length, 2);
+        assert.equal(this.img.length, 2);
 
-        console.log(this.img.actions);
-    }
-
-    , cropChainedInvalid: function(){
-        assert.throws(function(){
-            this.img.crop({top: 400, left: 400, width: 500})
-        }.bind(this), Error);
+        this.img.toBuffer(null, null, function(error){
+            assert.throws(function(){
+                throw error;
+            },
+            'Image data too small')
+        });
     }
 };
 
 testCases.callstack = {
     _setup: function(){
-        this.cstack = new CallStack();
+        this.cstack = new FluentInterface();
+    }
+    , initialTest: function(){
+        assert.strictEqual(false, this.cstack.hasNext());
+        assert.strictEqual(0, this.cstack.length);
+    }
+    , testRepetition: function(){
+        var counter = 0;
+        this.cstack.push(function(){
+            counter++;
+            this._callNext();
+        });
+        this.cstack.push(function(){
+            counter++;
+            this._callNext();
+        });
+        this.cstack.execute(function(){
+            counter++;
+            assert.strictEqual(3, counter);
+        });
+    }
+    , testArguments: function(){
+        this.cstack.push(function(counter){
+            counter++;
+            assert.strictEqual(3, counter);
+            this._callNext('test', 10);
+        });
+        this.cstack.push(function(name, number){
+            assert.strictEqual(10, number);
+            assert.strictEqual('test', name);
+            this._callNext(true);
+        });
+        this.cstack.execute(function(boolean){
+            assert.strictEqual(true, boolean);
+        }, 2);
+    }
+    , testEmpty: function(){
+        this.cstack.execute(function(name){
+            assert.strictEqual('yeah', name);
+        }, 'yeah');
     }
 };
 
@@ -69,7 +107,8 @@ for(var name in testCases){
                 testCases[name][test].call(testCases[name]);
                 log.info('['+test+'] executed successfully.');
             } catch (e) {
-                log.error('['+test+'] failed with message: "'+ e.message+'"')
+                log.error('['+test+'] failed:');
+                log(e);
             }
         }
     }
