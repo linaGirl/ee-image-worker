33d71e1ccbc7e726c9b29f2b8111d4bc91cc2480
Michael Rüfenacht <ruefe@rcky.ch>
2013-12-05 15:59:22 +0100

    Fixed most of the padding concerns. What is missing, is color conversion if the passed padding color contains more channels than the image itself.

--- index.js
+++ index.js
@@ -5,67 +5,12 @@ var WorkerImage = require('./lib/WorkerImage'),
 
 module.exports = require('./lib/ImageWorker');
 
-fs.readFile('./images/theband.jpg', function(err, buffer){
-//    picha.decode(buffer, function(err, result){
-//        picha.resize(result, {width: 456, height: 300, filter: 'lanczos'}, function(err, crop){
-//            log(crop);
-//            var sub = crop.subView(77, 0, 300, 300);
-//            var decoded = picha.encodeJpegSync(sub);
-//            fs.writeFile('./images/theband_crop.jpg', decoded, function(error){
-//                log(error);
-//            });
-//        });
-//    });
+fs.readFile('./images/padded.png', function(err, buffer){
     var wi = new WorkerImage(buffer);
-    wi.pad({top: 100 }).crop({top: 400, left: 300}).resize({width: 100, height: 100}).toBuffer(function(err, data){
+    wi.pad({left:30, color:[0xFF, 0x00, 0x00]}).toBuffer('png', function(err, data){
         log(err);
-        fs.writeFile('./images/theband_fit.jpg', data, function(error){
+        fs.writeFile('./images/padded_padded.png', data, function(error){
             log(error);
         });
     });
 });
-//fs.readFile('./images/theband.jpg', function(err, buffer){
-//    picha.decode(buffer, function(err, result){
-//        var additional = 400,
-//            rows = [],
-//            psize = 4,
-//            stride = ((1615 * psize + 3) & ~3),
-//            padded;
-//        var converted = picha.colorConvertSync(result, {pixel: 'rgba'});
-//        var iterations = converted.data.length / converted.stride;
-//
-//        var emptyLine = new Buffer(stride).fill(0x0);
-//        for(var i=0; i<100; i++){
-//            rows.push(emptyLine);
-//        }
-//
-//        var left = new Buffer(200*psize).fill(0x0);
-//        var right = new Buffer(stride - converted.width * psize - 200 * psize).fill(0x0);
-//        for(var i=0; i < iterations; i++){
-//            // row does not include the padding!
-//            rows.push(Buffer.concat([left, converted.row(i), right]));
-//        }
-//
-//        for(var i=0; i<100; i++){
-//            rows.push(emptyLine);
-//        }
-//        padded = Buffer.concat(rows);
-//        var pi = new picha.Image({width:1615, height: 1000, data: padded, pixel: 'rgba'});
-//        picha.encodePng(pi, function(error, data){
-//            console.log(error);
-//            fs.writeFile('./images/padded.png', data, function(err){
-//                if(!err){
-//                    console.log('Yeah');
-//                }
-//            });
-//        });
-//        console.log(padded.length);
-//        var pi = new picha.Image({width:1615, height: 800, stride:4848, pixel: 'rgb', data: padded});
-//
-//    });
-//});
-/**fs.readFile('./images/theband.jpg', function(err, buffer){
-    var img = picha.decode(buffer, function(err, result){
-       console.log(result.data.length);
-    });
-});*/
