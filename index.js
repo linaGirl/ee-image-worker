07e3695564dd611ad9cbc9c58b0f3cc328ca4ca2
Michael van der Weg <michael@joinbox.com>
2014-05-26 13:40:22 +0200

    exporting the main class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var WorkerImage = require('./lib/WorkerImage');
 
+module.exports = WorkerImage;
 module.exports.ImageWorker = require('./lib/ImageWorker');
 module.exports.WorkerImage = WorkerImage;
