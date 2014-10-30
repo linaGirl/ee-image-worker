5ba5d7a2c47e4f147b50119aeaab8b30835b5c10
mruefenacht <ruefe@rcky.ch>
2014-10-30 16:23:37 +0100

    Added an example and added a package interface.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// @todo: create an environment which acts as a factory for images and transformations (and resolves the chosen engine)
-module.exports = require('./lib/Image');
+var   Image             = require('./lib/Image')
+    , Transformation    = require('./lib/Transformation')
+    , engines           = require('./lib/engine')
+    , errors            = require('./lib/Error');
+
+var defaultEngine = new engines.PichaEngine();
+
+module.exports.createImage = function(buffer, engine){
+    return new Image(buffer, engine || defaultEngine);
+};
+module.exports.createTransformation = function(engine){
+    return new Transformation(engine || defaultEngine);
+};
+
+module.exports.errors = errors;
